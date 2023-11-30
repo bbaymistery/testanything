91b8eaf03d696906471afbeff317ff8d486ae00e
Elgun <elgun.ezmemmedov@mail.ru>
2023-11-30 14:19:15 +0400

    's'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,12 @@ import "../styles/global.scss";
 import { useRouter } from 'next/router';
 import { extractLanguage } from '../helpers/extractLanguage';
 import { checkLanguageAttributeOntheUrl } from '../helpers/checkLanguageAttributeOntheUrl';
-
+import { Jost } from '@next/font/google'
+const font = Jost({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+  display: 'swap', // Add this line
+})
 export const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
   const dispatch = useDispatch()
@@ -110,7 +115,7 @@ export const MyApp = ({ Component, pageProps }) => {
 
 
   return (<Provider store={store}>
-    <main >
+    <main className={`${router.pathname === '/' ? font.className : ""}`}>
       <Component {...pageProps} />
     </main>
   </Provider>);
