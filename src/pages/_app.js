d7bbb3a2c65685a2dc2837098649d30f51d76887
Elgun <elgun.ezmemmedov@mail.ru>
2023-11-30 13:57:43 +0400

    's'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ import { Jost } from '@next/font/google'
 const font = Jost({
   weight: ['400', '500'],
   subsets: ['latin'],
-  display: 'fallback', // Add this line
+  display: 'swap', // Add this line
 })
 export const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
