cc128ae7ced04876840507b33b69f4d04d5143b4
Elgun <elgun.ezmemmedov@mail.ru>
2023-12-01 12:35:51 +0400

    's'

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,44 +5,12 @@ import CarsSlider from "../components/widgets/CarsSlider";
 import Tours from "./tours";
 import Testimonials from "../components/widgets/Testimonials";
 import dynamic from 'next/dynamic'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const TaxiDeals = dynamic(
   () => import('../components/widgets/TaxiDeals'),
   { loading: () => <div>Loading...</div> } // Replace this with your custom loading component
 );
 export default function Home(props) {
-  // const [countryCode, setCountryCode] = useState(null);
-
-  // useEffect(() => {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(showPosition, handleError);
-  //   }
-
-  //   async function showPosition(position) {
-  //     const lat = position.coords.latitude;
-  //     const lon = position.coords.longitude;
-
-  //     // Use the latitude and longitude to fetch the countryCode
-  //     const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
-  //     const data = await response.json();
-
-  //     setCountryCode(data.address.country_code);
-  //   }
-
-  //   function handleError(error) {
-  //     switch (error.code) {
-  //       case error.PERMISSION_DENIED:
-  //       case error.POSITION_UNAVAILABLE:
-  //       case error.TIMEOUT:
-  //       case error.UNKNOWN_ERROR:
-  //       default:
-  //         setCountryCode("gb");
-  //         break;
-  //     }
-  //   }
-
-  // }, []);
-  // console.log(`Country is : ${countryCode} `);
   const [hasScrolled, setHasScrolled] = useState(false);
   const handleScroll = () => {
     if (!hasScrolled) {
@@ -59,7 +27,9 @@ export default function Home(props) {
     <GlobalLayout footerbggray={true}>
       <Hero />
       <TaxiDeals />
-      <SeaportTransfers bggray={true} />
+
+      {hasScrolled && <SeaportTransfers bggray={true} />}
+
       {hasScrolled && <Tours insideGlobalLayout={false} />}
       {hasScrolled && <CarsSlider bggray={true} />}
       <Testimonials bggray={false} />
