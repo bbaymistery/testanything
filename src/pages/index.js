6f9bdd404dfeba2f000832d6e3766da5c14c4135
Elgun <elgun.ezmemmedov@mail.ru>
2023-12-01 12:26:24 +0400

    's'

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,58 @@ const TaxiDeals = dynamic(
   { loading: () => <div>Loading...</div> } // Replace this with your custom loading component
 );
 export default function Home(props) {
+  // const [countryCode, setCountryCode] = useState(null);
+
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(showPosition, handleError);
+  //   }
+
+  //   async function showPosition(position) {
+  //     const lat = position.coords.latitude;
+  //     const lon = position.coords.longitude;
+
+  //     // Use the latitude and longitude to fetch the countryCode
+  //     const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
+  //     const data = await response.json();
+
+  //     setCountryCode(data.address.country_code);
+  //   }
+
+  //   function handleError(error) {
+  //     switch (error.code) {
+  //       case error.PERMISSION_DENIED:
+  //       case error.POSITION_UNAVAILABLE:
+  //       case error.TIMEOUT:
+  //       case error.UNKNOWN_ERROR:
+  //       default:
+  //         setCountryCode("gb");
+  //         break;
+  //     }
+  //   }
+
+  // }, []);
+  // console.log(`Country is : ${countryCode} `);
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const handleScroll = () => {
+    if (!hasScrolled) {
+      setHasScrolled(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    // Clean up the event listener on component unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [hasScrolled]); // Add hasScrolled as a dependency to avoid unnecessary re-registrations
 
   return (
     <GlobalLayout footerbggray={true}>
       <Hero />
       <TaxiDeals />
       <SeaportTransfers bggray={true} />
-      <Tours insideGlobalLayout={false} />
-
+      {hasScrolled && <Tours insideGlobalLayout={false} />}
+      {hasScrolled && <CarsSlider bggray={true} />}
+      <Testimonials bggray={false} />
     </GlobalLayout>
   )
 }
--- src/pages/tours/index.js
+++ src/pages/tours/index.js
@@ -126,8 +126,6 @@ const Tours = (props) => {
                             }
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
