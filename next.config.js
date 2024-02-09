8f828bdfeebed66b1225449836d18ff1803712a3
Elgun <elgun.ezmemmedov@mail.ru>
2024-02-09 14:30:00 +0400

    's'

--- next.config.js
+++ next.config.js
@@ -11,9 +11,7 @@ module.exports = {
     mapApiKey: "AIzaSyDulwIwncfuxBve8MKXPIIPmPLRve6ySw8",
     NEXT_PUBLIC_GOOGLE_ANALYTICS: "G-S02J90JMSB",
   },
-  experimental: {
-    nextScriptWorkers: true,
-  },
+
   async redirects() {
     return [
       //! redirect for home page is started
