9faa1538020661b736c212c2fc6fc1a78e347daf
Elgun <elgun.ezmemmedov@mail.ru>
2024-02-12 07:24:08 +0400

    's'

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,13 +40,13 @@ class CustomDocument extends Document {
           {/* <link rel="stylesheet" href="/fontawesome/css/all.min.css" /> */}
           <meta name="keywords" content={keywords} />
           {/* LAZY LOAD ILE DE DENE */}
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-54500Y5330" strategy='beforeInteractive' />
+          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-54500Y5330" />
           <Script strategy='beforeInteractive'>{`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-54500Y5330');
-            `}</Script>
+            `}</Script> */}
 
 
 
